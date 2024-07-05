9b850b5f1bbaf256e134a61ced19c5f634976c90
Juan <jouglardjuan@gmail.com>
2024-07-05 13:42:55 -0300

    debugging issue with s3 endpoint

--- server/services/file.py
+++ server/services/file.py
@@ -3,6 +3,9 @@ import uuid
 from db.models import File, FileData
 from .analyzer import parse_file
 import boto3
+import requests
+from time import time
+
 
 def validate(file: UploadFile):
     print(f"File: {file.content_type}")
@@ -33,17 +36,37 @@ async def process_file(lines: str):
 
 async def save_file_on_bucket():
     BUCKET_NAME = "whatsapp-convs"
-    AWS_ACCESS_KEY_ID = 'VPP0fkoCyBZx8YU0QTjH'
-    AWS_SECRET_KEY_ID = 'iFq6k8RLJw5B0faz0cKCXeQk0w9Q8UdtaFzHuw4J'
-    ENDPOINT="https://s3_emulator:9000"
+    AWS_ACCESS_KEY_ID = 'Jw0KBBTTnInkOiskepJJ'
+    AWS_SECRET_KEY_ID = 'dBjOipYzMbrReI6n72E2q7vgl9XpRvzD526NxpYK'
+    ENDPOINT="http://s3_emulator:9001"
 
     print("Starting upload")
     s3_client = boto3.client(
             "s3",
-            aws_access_key_id=AWS_SECRET_KEY_ID,
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_KEY_ID,
-            endpoint_url=ENDPOINT
             )
     s3_client.put_object(Key="file.py", Bucket=BUCKET_NAME, Body="some data")
     print("File uploaded")
 
+
+def ping_url(url):
+    try:
+        start_time = time()
+        response = requests.get(url)
+        end_time = time()
+        response_time = end_time - start_time
+        if response.status_code == 200:
+            print(f"URL: {url} is reachable. Response time: {response_time:.2f} seconds.")
+        else:
+            print(f"URL: {url} returned status code {response.status_code}.")
+    except requests.exceptions.RequestException as e:
+        print(f"URL: {url} is not reachable. Error: {e}")
+
+
+
+def is_valid_ipv6_endpoint_url(endpoint_url):
+    if UNSAFE_URL_CHARS.intersection(endpoint_url):
+        return False
+    hostname = f'[{urlparse(endpoint_url).hostname}]'
+    return IPV6_ADDRZ_RE.match(hostname) is not None
