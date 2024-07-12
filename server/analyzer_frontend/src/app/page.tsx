8b7a0a6f4b6395de367cb078a7914c31c25243de
Juan <jouglardjuan@gmail.com>
2024-07-12 20:04:01 -0300

    components to show buckets and files

--- server/analyzer_frontend/src/app/page.tsx
+++ server/analyzer_frontend/src/app/page.tsx
@@ -1,19 +1,15 @@
 "use server"
-
-import Image from "next/image";
 import MinioService from './services/minio'
+import Home from "./home"
 
 const minioService = new MinioService()
 
-export default async function Home() {
+export default async function Main() {
   const files = await minioService.get_buckets()
   console.log(`Files - MINIO: ${JSON.stringify( files )}`)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <ul>
-            {files.map((file) => <li key={ file.name }>{file.name}</li>)}
-        </ul>
     </main>
   );
 }
