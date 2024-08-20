3c347ef0cb915bee4c1106bd5dc77f9e8f94713c
Juan <jouglardjuan@gmail.com>
2024-08-20 11:46:06 -0300

    use 'react' branch in frontend

--- server/analyzer_frontend/clone_and_run.sh
+++ server/analyzer_frontend/clone_and_run.sh
@@ -1,12 +1,10 @@
-#/bin/sh
-
 eval $(ssh-agent)
 
 chmod 600 /root/.ssh/id_ed25519
 
 ssh-add /root/.ssh/id_ed25519
 
-git clone git@github.com:JuanJouglard/whatsapp-analyzer-frontend.git
+git clone -b react git@github.com:JuanJouglard/whatsapp-analyzer-frontend.git
 
 cd whatsapp-analyzer-frontend
 
